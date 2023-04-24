8e933f201c8db397eda83bbc74b647cff368742b
梅启峰 <mqflive@qq.com>
2023-04-24 13:16:03 +0800

    添加网站的访问量和优化描述

--- scripts/downFile.js
+++ scripts/downFile.js
@@ -49,4 +49,28 @@ $(function () {
         //浏览器打开新的页面
         window.open('https://startgame.lanzouo.com/' + url);
     })
-});
+
+    //http 请求
+    function httpGet(url, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.responseType = "json";
+        xhr.onload = function () {
+            if (this.status == 200) {
+                if (callback) callback(this.response);
+            }
+        };
+        xhr.send();
+    }
+
+    httpGet('https://5r4dqyqvtg.hk.aircode.run/addstatistics', function (data) {
+        console.log(data);
+    })
+
+    httpGet('https://5r4dqyqvtg.hk.aircode.run/getvisits', function (data) {
+        //给span标签赋值
+        $('#today').text('今日访问：' + data.today)
+        $('#total').text('本站总访问：' + data.total);
+    }
+    )
+})
